d6aab43e96520b2b55d9514c399305696ae012ab
Utaa <adamfachreza15@gmail.com>
2022-08-03 22:53:22 +0700

    create updateOrderRequest

--- app/Http/Requests/Dashboard/MyOrder/UpdateMyOrderRequest.php
+++ app/Http/Requests/Dashboard/MyOrder/UpdateMyOrderRequest.php
@@ -13,7 +13,7 @@ class UpdateMyOrderRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,27 @@ class UpdateMyOrderRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'buyer_id' => [
+                'nullable','integer',
+            ],
+            'freelabcer_id' => [
+                'nullable','integer',
+            ],
+            'service_id' => [
+                'nullable','integer',
+            ],
+            'file' => [
+                'nullable','mimes:zip','max:1024',
+            ],
+            'note' => [
+                'required','string','max:10000',
+            ],
+            'expired' => [
+                'nullable','date',
+            ],
+            'order_status_id' => [
+                'nullable', 'integer',
+            ],
         ];
     }
 }
